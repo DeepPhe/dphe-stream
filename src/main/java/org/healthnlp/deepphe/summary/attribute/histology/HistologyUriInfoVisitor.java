3695a06f0a805d8fe60911e7d17ee490dc11a1fa
Sean Finan <sean.finan@childrens.harvard.edu>
2022-03-31 13:14:13 -0400

    If a candidate diagnosis is preceded by the text "histologic type:" then that candidate should be favored.

--- src/main/java/org/healthnlp/deepphe/summary/attribute/histology/HistologyUriInfoVisitor.java
+++ src/main/java/org/healthnlp/deepphe/summary/attribute/histology/HistologyUriInfoVisitor.java
@@ -2,6 +2,9 @@ package org.healthnlp.deepphe.summary.attribute.histology;
 
 import org.healthnlp.deepphe.neo4j.constant.UriConstants;
 import org.healthnlp.deepphe.neo4j.embedded.EmbeddedConnection;
+import org.healthnlp.deepphe.neo4j.node.Mention;
+import org.healthnlp.deepphe.neo4j.node.Note;
+import org.healthnlp.deepphe.node.NoteNodeStore;
 import org.healthnlp.deepphe.summary.attribute.infostore.UriInfoVisitor;
 import org.healthnlp.deepphe.summary.concept.ConceptAggregate;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -17,6 +20,7 @@ final public class HistologyUriInfoVisitor implements UriInfoVisitor {
 
    private Collection<ConceptAggregate> _histologyConcepts;
 
+   static private final int HISTOLOGY_WINDOW = 23;
    @Override
    public Collection<ConceptAggregate> getAttributeConcepts( final Collection<ConceptAggregate> neoplasms ) {
       if ( _histologyConcepts == null ) {
@@ -51,6 +55,37 @@ final public class HistologyUriInfoVisitor implements UriInfoVisitor {
 //                  .flatMap( Collection::stream )
 //                  .filter( c -> !c.isNegated() )
 //                  .forEach( _histologyConcepts::add );
+
+
+         //  Added 3/31/2022
+         //  If text contains "histologic type: [type]" for any detected aggregates only those are returned.
+         final Collection<ConceptAggregate> histologies = new HashSet<>();
+         for ( ConceptAggregate aggregate : _histologyConcepts ) {
+            for ( Mention mention : aggregate.getMentions() ) {
+               final int mentionBegin = mention.getBegin();
+               if ( mentionBegin <= HISTOLOGY_WINDOW ) {
+                  continue;
+               }
+               final Note note = NoteNodeStore.getInstance().get( mention.getNoteId() );
+               if ( note == null ) {
+//                  LOGGER.warn( "No Note stored for Note ID " + mention.getNoteId() );
+                  continue;
+               }
+               if ( note.getText()
+                        .substring( mentionBegin-HISTOLOGY_WINDOW, mentionBegin )
+                        .toLowerCase()
+                        .contains( "histologic type:" ) ) {
+                  histologies.add( aggregate );
+                  break;
+               }
+            }
+         }
+         if ( !histologies.isEmpty() ) {
+            _histologyConcepts.retainAll( histologies );
+         }
+
+
+
       }
       return _histologyConcepts;
    }
